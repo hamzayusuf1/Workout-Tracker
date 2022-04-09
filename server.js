1cf3a9d1f1094a841710de7a8f8d5792aa6dfb42
Allure <samira-h@hotmail.co.uk>
2022-04-09 11:15:46 +0100

    edits to dependencies and server

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
-const path = require("path")
-const express = require("express");
-const session = require("express-session")
-const app = express();
-const PORT = process.env.PORT || 3000;
+// Dependencies
+const path = require('path');
+require('dotenv').config();
+const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
-app.listen(() => {
-    console.log(`App listening on PORT:" ${PORT}`)
-})
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session(sess));
+
+
+app.use(routes);
+
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
