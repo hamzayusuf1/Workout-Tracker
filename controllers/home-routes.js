b6236aea7f38269282a74ba92eeed43d9db8b65a
Allure <samira-h@hotmail.co.uk>
2022-04-14 21:37:47 +0100

    gets to homepage and recent post

---
+++ controllers/home-routes.js
@@ -0,0 +1,31 @@
+
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+// Render the home page
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+          ],
+        // Order the posts from most recent to least
+        order: [[ 'created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
