3cc0c728845c33852a0e2855602d3efa01cbf6b6
Allure <samira-h@hotmail.co.uk>
2022-04-14 19:06:35 +0100

    get single users by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,3 +15,37 @@ router.get('/', (req, res) => {
       });
   });
 
+  // GET  -- get a single user by id
+router.get('/:id', (req, res) => {
+    User.findOne({
+      attributes: { exclude: ['password'] },
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'title', 'post_text', 'created_at']
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+              model: Post,
+              attributes: ['title']
+          }
+      }
+      ]
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'There are no users with this id found.' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
