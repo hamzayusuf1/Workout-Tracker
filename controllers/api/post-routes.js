3172b00266471802b2fc4866ab7106e9b9dd11f2
Allure <samira-h@hotmail.co.uk>
2022-04-14 21:52:07 +0100

    get post by id

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -36,3 +36,43 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// GET -- get a single post by id
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
