747305a31091bd35dc9f5215faf4f596704c413f
Allure <samira-h@hotmail.co.uk>
2022-04-14 19:07:32 +0100

    login in route for user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -71,3 +71,29 @@ router.post('/', (req, res) => {
       });
   });
 
+  // POST -- login route for a user
+router.post('/login',  (req, res) => {
+    User.findOne({
+        where: {
+        email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+        res.status(400).json({ message: 'There are no users with this email found!' });
+        return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });  
+});
+
