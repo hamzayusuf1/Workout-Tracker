a968d37b4fd50384a723796128ccaeb635c4b081
Allure <samira-h@hotmail.co.uk>
2022-04-14 19:06:05 +0100

    get all users

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { User, Post, Comment} = require('../../models');
+const session = require('express-session');
+const withAuth = require('../../utils/auth');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// GET -- get all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  });
+
